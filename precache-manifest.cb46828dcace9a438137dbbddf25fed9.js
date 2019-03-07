f592d7bee13d60ad90809f7e969055b46a925285
Fahad Khan Ashrafi <mfahadkhanashrafi@outlook.com>
2019-03-07 22:10:13 +0500

    Updates

---
+++ precache-manifest.cb46828dcace9a438137dbbddf25fed9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "76216daba447eeacc9d9",
+    "url": "/social/static/js/runtime~main.7eb00e2d.js"
+  },
+  {
+    "revision": "996d0fbfa7d0e0fe47ff",
+    "url": "/social/static/js/main.22c82188.chunk.js"
+  },
+  {
+    "revision": "83973ef178996a8e9f6f",
+    "url": "/social/static/js/2.b00817da.chunk.js"
+  },
+  {
+    "revision": "996d0fbfa7d0e0fe47ff",
+    "url": "/social/static/css/main.42470f14.chunk.css"
+  },
+  {
+    "revision": "77136ee3503540fc26d1047ed3d99aec",
+    "url": "/social/index.html"
+  }
+];
